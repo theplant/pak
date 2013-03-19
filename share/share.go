44cacdb6f25b9819774a3e0838c4d416b6d1256c
bom_d_van <bom.d.van@gmail.com>
2013-03-19 13:46:13 +0800

    use share package for package info sharing

---
+++ share/share.go
@@ -0,0 +1,26 @@
+package share
+
+import (
+	"os"
+)
+
+const (
+	Pakfile   = "Pakfile"
+	Paklock   = "Pakfile.lock"
+	Pakbranch = "pak"
+	Paktag    = "_pak_latest_"
+)
+
+var Gopath = os.Getenv("GOPATH")
+
+type PakInfo struct {
+	Packages []string
+}
+
+type PaklockInfo map[string]string
+
+type GetOption struct {
+	Fetch bool
+	Force       bool
+	Checksum    string
+}
