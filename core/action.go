5e6be134d2e591d743597bf78c7548ccf11a2bcb
bom_d_van <bom.d.van@gmail.com>
2013-04-10 15:32:30 +0800

    refactor core package

--- core/action.go
+++ core/action.go
@@ -21,11 +21,12 @@ func Init() error {
 }
 
 func Get(option PakOption) error {
-	// Parse
+	// Retrieve PakPkgs from Pakfile
 	allPakPkgs, err := ParsePakfile()
 	if err != nil {
 		return err
 	}
+
 	var paklockInfo PaklockInfo
 	newPaklockInfo := PaklockInfo{}
 	paklockInfo, err = GetPaklockInfo()
@@ -38,27 +39,64 @@ func Get(option PakOption) error {
 	} else {
 		newPaklockInfo = paklockInfo
 	}
+
 	if !option.UsePakfileLock {
 		paklockInfo = nil
 	}
 
 	// Assign GetOption && Sync && Report Erorrs
-	for i := 0; i < len(allPakPkgs); i++ {
-		// TODO: remove Fetch option
-		allPakPkgs[i].GetOption.Fetch = option.Fetch
-		allPakPkgs[i].GetOption.Force = option.Force
-		allPakPkgs[i].GetOption.NotGet = option.NotGet // added temporally. TODO: refactor
+	err = loadPkgs(&allPakPkgs, option)
+	if err != nil {
+	    return err
+	}
 
-		err = allPakPkgs[i].Sync()
+	// For: pak update package
+	// Pick up PakPkg to be updated this time
+	pakPkgs := []PakPkg{}
+	if len(option.PakMeter) != 0 {
+		var matched bool
+		var pakPkg PakPkg
+		for _, pakPkgName := range option.PakMeter {
+			matched, pakPkg, err = isPkgMatched(allPakPkgs, pakPkgName)
+			if err != nil {
+			    return err
+			}
+
+			if matched {
+				pakPkgs = append(pakPkgs, pakPkg)
+			} else {
+				return fmt.Errorf("Package %s Is Not Included in Pakfile.", pakPkgName)
+			}
+		}
+	} else {
+		pakPkgs = allPakPkgs
+	}
+
+	err = setDependencies(pakPkgs, paklockInfo, &newPaklockInfo)
+	if err != nil {
+	    return err
+	}
+
+	// TODO: Add Tests for Writing Pakfile.Lock
+	return writePaklockInfo(newPaklockInfo)
+}
+
+func loadPkgs(allPakPkgs *[]PakPkg, option PakOption) (err error) {
+	for i := 0; i < len((*allPakPkgs)); i++ {
+		// TODO: remove Fetch option
+		(*allPakPkgs)[i].GetOption.Fetch = option.Fetch
+		(*allPakPkgs)[i].GetOption.Force = option.Force
+
+		err = (*allPakPkgs)[i].Sync()
 		if err != nil {
 			// go get package when the package is not downloaded before
-			if !allPakPkgs[i].State.IsPkgExist {
-				err = allPakPkgs[i].GoGet()
+			if !(*allPakPkgs)[i].State.IsPkgExist {
+				err = (*allPakPkgs)[i].GoGet()
 				if err != nil {
 					return err
 				}
 
-				err = allPakPkgs[i].Sync()
+				err = (*allPakPkgs)[i].Sync()
 				if err != nil {
 					return err
 				}
@@ -67,84 +105,79 @@ func Get(option PakOption) error {
 			}
 		}
 
-		// TODO: add tests
-		// this enable pak to check pak even the local package repo doesn't contain
-		// the remote branch.
-		// for:
-		// 新bug：执行 pak get 时，Pak 没有先执行 fetch 就直接 checkout pak HASH，导致出现无法checkout的错误。重现方法：
-		// * 把package的远程分支删除 git -r -d origin/master
-		// * 回到项目执行 pak get
-		// * 出现错误：`github.com/xxx` does not contain reference `refs/remotes/origin/master`
-		//
-		err = allPakPkgs[i].GitPkg.Fetch()
+		// fetch before hand can make sure that the package contains up-to-date remote branch
+		err = (*allPakPkgs)[i].GitPkg.Fetch()
 		if err != nil {
 			return err
 		}
 
-		allPakPkgs[i].GitPkg.Sync()
+		(*allPakPkgs)[i].GitPkg.Sync()
 
-		err = allPakPkgs[i].Report()
+		err = (*allPakPkgs)[i].Report()
 		if err != nil {
 			return err
 		}
 	}
 
-	// For: pak update package
-	// Pick up PakPkg to be updated this time
-	pakPkgs := []PakPkg{}
-	if len(option.PakMeter) != 0 {
-		for _, pakPkgName := range option.PakMeter {
-			described := false
-			// full-name matching
-			for _, pakPkg := range allPakPkgs {
-				if pakPkg.Name == pakPkgName {
-					pakPkgs = append(pakPkgs, pakPkg)
-					described = true
-					break
-				}
-			}
-			// partial matching
-			if !described {
-				matchedResult := []string{}
-				for _, pakPkg := range allPakPkgs {
-					pakPkgNameReg := regexp.MustCompile(pakPkgName)
-					if pakPkgNameReg.MatchString(pakPkg.Name) {
-						pakPkgs = append(pakPkgs, pakPkg)
-						described = true
+	return
+}
 
-						matchedResult = append(matchedResult, pakPkg.Name)
-					}
-				}
-				if len(matchedResult) > 1 {
-					nameString := ""
-					for _, pkg := range matchedResult {
-						nameString = fmt.Sprintf("%s    %s\n", nameString, pkg)
-					}
-					return fmt.Errorf("More than 1 matched packages:\n%sCan't update with ambiguous package matching.", nameString)
-				}
-			}
-
-			// non-matching package
-			if !described {
-				return fmt.Errorf("Package %s Is Not Included in Pakfile.", pakPkgName)
-			}
+func isPkgMatched(allPakPkgs []PakPkg, pakPkgName string) (bool, PakPkg, error) {
+	matched := false
+	matchedPakPkg := PakPkg{}
+	// full-name matching
+	for _, pakPkg := range allPakPkgs {
+		if pakPkg.Name == pakPkgName {
+			// pakPkgs = append(pakPkgs, pakPkg)
+			matchedPakPkg = pakPkg
+			matched = true
+			break
 		}
-	} else {
-		pakPkgs = allPakPkgs
 	}
 
-	// Categorize Pakpkgs
+	// partial matching
+	if !matched {
+		matchedResult := []string{}
+		for _, pakPkg := range allPakPkgs {
+			pakPkgNameReg := regexp.MustCompile(pakPkgName) // TODO: should not use MustCompile here.
+			if pakPkgNameReg.MatchString(pakPkg.Name) {
+				// pakPkgs = append(pakPkgs, pakPkg)
+				matchedPakPkg = pakPkg
+				matched = true
+
+				matchedResult = append(matchedResult, pakPkg.Name)
+			}
+		}
+
+		if len(matchedResult) > 1 {
+			nameString := ""
+			for _, pkg := range matchedResult {
+				nameString = fmt.Sprintf("%s    %s\n", nameString, pkg)
+			}
+
+			err := fmt.Errorf("More than 1 matched packages:\n%sStop updating for ambiguous package matching.", nameString)
+			return false, matchedPakPkg, err
+		}
+	}
+
+	return matched, matchedPakPkg, nil
+}
+
+func setDependencies(pakPkgs []PakPkg, paklockInfo PaklockInfo, newPaklockInfo *PaklockInfo) error {
 	newPakPkgs, toUpdatePakPkgs, toRemovePakPkgs := ParsePakState(pakPkgs, paklockInfo)
 
-	// Pak dependencies
-	var checksum string
+	var (
+		 checksum string
+		 err error
+	)
+
 	for i := 0; i < len(newPakPkgs); i++ {
 		checksum, err = newPakPkgs[i].Pak(newPakPkgs[i].GetOption)
 		if err != nil {
 			return err
 		}
 
-		newPaklockInfo[newPakPkgs[i].Name] = checksum
+		(*newPaklockInfo)[newPakPkgs[i].Name] = checksum
 	}
 	for i := 0; i < len(toUpdatePakPkgs); i++ {
 		checksum, err = toUpdatePakPkgs[i].Pak(toUpdatePakPkgs[i].GetOption)
@@ -152,7 +185,7 @@ func Get(option PakOption) error {
 			return err
 		}
 
-		newPaklockInfo[toUpdatePakPkgs[i].Name] = checksum
+		(*newPaklockInfo)[toUpdatePakPkgs[i].Name] = checksum
 	}
 	for i := 0; i < len(toRemovePakPkgs); i++ {
 		err = toRemovePakPkgs[i].Unpak(toRemovePakPkgs[i].Force)
@@ -161,9 +194,8 @@ func Get(option PakOption) error {
 		}
 
 		// TODO: Add tests for removing Pakfile.lock record
-		delete(newPaklockInfo, toRemovePakPkgs[i].Name)
+		delete((*newPaklockInfo), toRemovePakPkgs[i].Name)
 	}
 
-	// TODO: Add Tests for Writing Pakfile.Lock
-	return writePaklockInfo(newPaklockInfo)
+	return err
 }
--- core/action_test.go
+++ core/action_test.go
@@ -10,26 +10,41 @@ import (
 
 type GetSuite struct {
 	pakPkgs []PakPkg
+	originalGoGetImpl func(name string) error
 }
 
 var _ = Suite(&GetSuite{})
 
-func mustRun(cmd *exec.Cmd) {
+func mustRun(params... string) (cmd *exec.Cmd) {
+	cmd = exec.Command(params[0], params[1:]...)
+
 	stderr := &bytes.Buffer{}
 	cmd.Stderr = stderr
 	err := cmd.Run()
 	if err != nil {
-		fp(stderr.String())
+		fl(stderr.String())
+
 		panic(err)
 	}
+
+	return
+}
+
+func (s *GetSuite) SetUpSuite(c *C) {
+	s.originalGoGetImpl = gitpkg.GoGetImpl
+	gitpkg.GoGetImpl = func(name string) error { return nil }
+}
+
+func (s *GetSuite) TearDownSuite(c *C) {
+	gitpkg.GoGetImpl = s.originalGoGetImpl
 }
 
 func (s *GetSuite) SetUpTest(c *C) {
-	mustRun(exec.Command("git", "clone", "fixtures/package1", "../../package1"))
-	mustRun(exec.Command("git", "clone", "fixtures/package2", "../../package2"))
-	mustRun(exec.Command("sh", "-c", "cd ../../package1 && git checkout -b pak && git checkout master && git tag _pak_latest_"))
-	mustRun(exec.Command("git", "clone", "fixtures/package3", "../../package3"))
-	mustRun(exec.Command("cp", "fixtures/Pakfile3", "Pakfile"))
+	mustRun("git", "clone", "fixtures/package1", "../../package1")
+	mustRun("git", "clone", "fixtures/package2", "../../package2")
+	mustRun("sh", "-c", "cd ../../package1 && git checkout -b pak && git checkout master && git tag _pak_latest_")
+	mustRun("git", "clone", "fixtures/package3", "../../package3")
+	mustRun("cp", "fixtures/Pakfile3", "Pakfile")
 
 	s.pakPkgs = []PakPkg{}
 	pkgsFixtures := [][]string{
@@ -43,11 +58,11 @@ func (s *GetSuite) SetUpTest(c *C) {
 }
 
 func (s *GetSuite) TearDownTest(c *C) {
-	mustRun(exec.Command("rm", "-rf", "../../package1"))
-	mustRun(exec.Command("rm", "-rf", "../../package2"))
-	mustRun(exec.Command("rm", "-rf", "../../package3"))
-	mustRun(exec.Command("rm", "-rf", "Pakfile"))
-	mustRun(exec.Command("rm", "-rf", "Pakfile.lock"))
+	mustRun("rm", "-rf", "../../package1")
+	mustRun("rm", "-rf", "../../package2")
+	mustRun("rm", "-rf", "../../package3")
+	mustRun("rm", "-rf", "Pakfile")
+	// mustRun("rm", "-rf", "Pakfile.lock")
 }
 
 func (s *GetSuite) TestGet(c *C) {
@@ -56,7 +71,6 @@ func (s *GetSuite) TestGet(c *C) {
 		UsePakfileLock: true,
 		Fetch:          true,
 		Force:          false,
-		NotGet:         true,
 	})
 	c.Check(err, Equals, nil)
 
@@ -68,25 +82,25 @@ func (s *GetSuite) TestGet(c *C) {
 	c.Check(s.pakPkgs[2].HeadRefsName, Equals, "refs/heads/pak")
 }
 
-// func (s *GetSuite) TestCanGetPackageWithoutRemoteBranch(c *C) {
-// 	pakPkgs[1].
-//
-// 	err := Get(PakOption{
-// 		PakMeter:       []string{},
-// 		UsePakfileLock: true,
-// 		Fetch:          true,
-// 		Force:          false,
-// 		NotGet:         true,
-// 	})
-// 	c.Check(err, Equals, nil)
-//
-// 	pakPkgs[0].Sync()
-// 	pakPkgs[1].Sync()
-// 	pakPkgs[2].Sync()
-// 	c.Check(pakPkgs[0].HeadRefsName, Equals, "refs/heads/pak")
-// 	c.Check(pakPkgs[1].HeadRefsName, Equals, "refs/heads/pak")
-// 	c.Check(pakPkgs[2].HeadRefsName, Equals, "refs/heads/pak")
-// }
+func (s *GetSuite) TestCanGetPackageWithoutRemoteBranch(c *C) {
+	mustRun("git", "--git-dir=../../package1/.git", "--work-tree=../../package1", "branch", "-dr", "origin/dev")
+	mustRun("git", "--git-dir=../../package1/.git", "--work-tree=../../package1", "branch", "-dr", "origin/master")
+
+	err := Get(PakOption{
+		PakMeter:       []string{},
+		UsePakfileLock: true,
+		Fetch:          true,
+		Force:          false,
+	})
+	c.Check(err, Equals, nil)
+
+	s.pakPkgs[0].Sync()
+	s.pakPkgs[1].Sync()
+	s.pakPkgs[2].Sync()
+	c.Check(s.pakPkgs[0].HeadRefsName, Equals, "refs/heads/pak")
+	c.Check(s.pakPkgs[1].HeadRefsName, Equals, "refs/heads/pak")
+	c.Check(s.pakPkgs[2].HeadRefsName, Equals, "refs/heads/pak")
+}
 
 func (s *GetSuite) TestGetWithPakMeter(c *C) {
 	err := Get(PakOption{
@@ -94,7 +108,6 @@ func (s *GetSuite) TestGetWithPakMeter(c *C) {
 		UsePakfileLock: true,
 		Fetch:          true,
 		Force:          false,
-		NotGet:         true,
 	})
 	c.Check(err, Equals, nil)
 
@@ -104,4 +117,71 @@ func (s *GetSuite) TestGetWithPakMeter(c *C) {
 	c.Check(s.pakPkgs[0].HeadRefsName, Equals, "refs/heads/master")
 	c.Check(s.pakPkgs[1].HeadRefsName, Equals, "refs/heads/pak")
 	c.Check(s.pakPkgs[2].HeadRefsName, Equals, "refs/heads/master")
+}
+
+func (s *GetSuite) TestPaklockInfoShouldUpdateAfterGet(c *C) {
+ 	expectedPaklockInfo	:= PaklockInfo{
+		"github.com/theplant/package3": "d5f51ca77f5d4f37a8105a74b67d2f1aefea939c",
+		"github.com/theplant/package1": "11b174bd5acbf990687e6b068c97378d3219de04",
+		"github.com/theplant/package2": "941af3b182a1d0a5859fd451a8b5a633f479d7bc",
+	}
+
+	err := Get(PakOption{
+		PakMeter:       []string{},
+		UsePakfileLock: false,
+		Fetch:          true,
+		Force:          false,
+	})
+	c.Check(err, Equals, nil)
+
+	paklockInfo, _ := GetPaklockInfo()
+	c.Check(len(paklockInfo), Equals, 3)
+	c.Check(paklockInfo, DeepEquals, expectedPaklockInfo)
+
+	err = Get(PakOption{
+		PakMeter:       []string{"github.com/theplant/package2"},
+		UsePakfileLock: false,
+		Fetch:          true,
+		Force:          false,
+	})
+	c.Check(err, Equals, nil)
+
+	paklockInfo, _ = GetPaklockInfo()
+	c.Check(len(paklockInfo), Equals, 3)
+	c.Check(paklockInfo, DeepEquals, expectedPaklockInfo)
+}
+
+func (s *GetSuite) TestGoGetAndFetchBeforePak(c *C) {
+	originalGoGetImpl := gitpkg.GoGetImpl
+	gogetPkg1Count := 0
+	gitpkg.GoGetImpl = func(name string) error {
+		if name == "github.com/theplant/package1" {
+			gogetPkg1Count++
+			if gogetPkg1Count == 1 {
+				mustRun("git", "clone", "fixtures/package1", "../../package1")
+			}
+		}
+
+		return nil
+	}
+	defer func() { gitpkg.GoGetImpl = originalGoGetImpl }()
+
+	mustRun("rm", "-rf", "../../package1")
+
+	err := Get(PakOption{
+		PakMeter:       []string{},
+		UsePakfileLock: true,
+		Fetch:          true,
+		Force:          false,
+	})
+	c.Check(err, Equals, nil)
+
+	c.Check(gogetPkg1Count, Equals, 2)
+
+	s.pakPkgs[0].Sync()
+	s.pakPkgs[1].Sync()
+	s.pakPkgs[2].Sync()
+	c.Check(s.pakPkgs[0].HeadRefsName, Equals, "refs/heads/pak")
+	c.Check(s.pakPkgs[1].HeadRefsName, Equals, "refs/heads/pak")
+	c.Check(s.pakPkgs[2].HeadRefsName, Equals, "refs/heads/pak")
 }
